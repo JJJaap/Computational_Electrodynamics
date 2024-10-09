39fad849989d54c5b4eecd9d0c513bd832ae471e
Jaap <104640027+JJJaap@users.noreply.github.com>
2024-10-09 21:08:13 +0200

    Wigner functions can now be generated for arbitrary functions (I think)

--- Wigner_Functions/Wigner_Distr.py
+++ Wigner_Functions/Wigner_Distr.py
@@ -1,47 +1,89 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors
+from scipy import signal
 
-def rect(x,X,a):
-	L = np.where(np.abs(x+X/2)>(a/2),0,1)
-	R = np.where(np.abs(x-X/2)>(a/2),0,1)
-	return L*R
+def rect(x,a):
+	return np.where(np.abs(x)>(a/2),0,1)
+
+def triangle(x,a):
+	return np.where(np.abs(x)>(a/2),0,-np.abs(x)+1)
+
+def squared_triangle(x,a):
+	return np.where(np.abs(x)>(a/2),0,-np.abs(x**2)+1)
+	
+def sin2(x,a):
+	return np.sin(a*x)**2
+
+def square_wave(x,a):
+	return signal.square(x)
 
 a = 1
-n = 1000
-x = np.linspace(-a,a,n)
+n = 10
+extx = 5*a
+extX = 5*a
 
-yffts = np.zeros((len(x),n)).astype(complex)
-xfft  = np.fft.fftshift(np.fft.fftfreq(n))
-Xs    = np.linspace(-a,a,n)
-ys    = np.zeros((len(x),n)).astype(complex)
+
+x     = np.linspace(-extx,extx,int(extx/a*n))
+Xs    = np.linspace(-extX,extX,int(extX/a*n))
+xfft  = np.fft.fftshift(np.fft.fftfreq(len(x)))
+ys    = np.zeros((len(Xs),len(x))).astype(complex)
+yffts = np.zeros((len(Xs),len(x))).astype(complex)
+
+
 
 i=0
 for X in Xs:
 
-	y          = rect(x,X,a)
+	y          = square_wave(x-X/2,a)*square_wave(x+X/2,a)#np.sin(x-X/2)*np.sin(x+X/2)
 	ys[i,:]    = y
 	yffts[i,:] = np.fft.fftshift(np.fft.fft(np.fft.fftshift(y),norm='forward'))
 
 	i+=1
 
-plt.plot(np.abs(yffts[int(n/2),:])**2)
-plt.show()
+xxs, XXs = np.meshgrid(x,Xs)
 
-plt.imshow(np.abs(yffts)**2)#,extent=[Xs[0],Xs[-1],xfft[-1],xfft[0]])
-#plt.imshow(np.where(np.abs(yffts)==0,1,0),extent=[xfft[0],xfft[-1],Xs[-1],Xs[0]])
+plt.imshow(np.real(ys),extent=[x[0],x[-1],Xs[-1],Xs[0]],cmap='BrBG')
+plt.xlabel("x'")
+plt.ylabel("X")
+plt.title("G(x',X)")
 plt.colorbar()
 plt.show()
 
+# plt.contour(xxs,XXs,np.real(ys))
+# plt.xlabel("x'")
+# plt.ylabel("X")
+# plt.title("G(x',X)")
+# plt.show()
 
-xxs, XXs = np.meshgrid(x,Xs)
+# plt.plot(np.abs(yffts[int(n/2),:])**2)
+# plt.show()
 
-xs, Xs = np.meshgrid(xfft, Xs)
-
-plt.contour(xxs,XXs,np.abs(ys)**2,[0])
+plt.imshow((np.abs(yffts)**2),extent=[xfft[0],xfft[-1],Xs[-1],Xs[0]],aspect=xfft[0]/Xs[0])#norm=colors.LogNorm()
+plt.colorbar()
+plt.xlabel("frequency along x (u)")
+plt.ylabel("X")
+plt.title("Wigner Function Magnitude (log scale)")
 plt.show()
 
-plt.contour(xs,Xs,np.abs(yffts)**2,[0])
+plt.imshow((np.angle(yffts)),extent=[xfft[0],xfft[-1],Xs[-1],Xs[0]],aspect=xfft[0]/Xs[0])
+plt.colorbar()
+plt.xlabel("frequency along x (u)")
+plt.ylabel("X")
+plt.title("Wigner Function Phase")
 plt.show()
+
+# plt.imshow((np.abs(yffts)**2),extent=[xfft[0],xfft[-1],Xs[-1],Xs[0]])
+# plt.colorbar()
+# plt.xlabel("frequency along x (u)")
+# plt.ylabel("X")
+# plt.show()
+
+
+
+# xs, Xs = np.meshgrid(xfft, Xs)
+# plt.contour(xs,Xs,np.abs(yffts)**2,[0])
+# plt.show()
 
 
 
